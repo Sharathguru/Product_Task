3902ec1bdc3c5e873a0eb96d639962ac508c2617
Sharathguru <vsarath200219@gmail.com>
2025-06-18 18:26:59 +0530

    update the render to the frontend

--- .history/Frontend/src/axios/axios_20250618172807.js
+++ .history/Frontend/src/axios/axios_20250618172807.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://product-task-4rq6.onrender.com/api/v1/product'
+    baseURL: 'http://localhost:5000/api/v1/product'
 });
 
 export default instance;
